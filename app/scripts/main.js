8f28c1ea7dbffd5c33a157b6100f6d3e83a400cc
Andrew El-Masry <andrewelmasrye@gmail.com>
2014-10-08 09:00:31 -0400

    Fixing gitignore

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('The Iron Yard Rocks');
+$(document).ready(function(){
+
+      var itemTitle;
+      var itemImage;
+      var itemPrice;
+      var itemShop;
+      var cont=$('.rightSide');
+
+
+      items.results.forEach(function (x) {
+        itemPrice=x.price + x.currency_code;
+
+        itemTitle="<li class='truncate'>"+x.title+"</li>";
+
+        itemShop=shop.shop_name;
+
+        x.Images.forEach(function (y) {
+        itemImage ='<img src = "' + y.url_170x135 + '">';
+
+        });
+
+        var allItems = "<div class='box'>" + itemTitle + itemImage + itemPrice + /*itemShop +*/ "</div>";
+          $('.rightSide').append(allItems);
+      });
+
+
+      });
